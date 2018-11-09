ac38c06cd9b4ab2b95554263ea9150fedbc482b9
satyamholmes <invinciblesatyam@gmail.com>
2018-11-09 08:38:31 +0530

    added basic layout for viewing picture and video

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var ex=require('express');
+var express=require('express');
 var fs=require('fs');
-var app=ex();
-var path="/media/satyam/funplace/megapixels/";
+var mime=require('mime')
+var app=express();
+var path="/media/satyam/funplace/megapixels";
 
 app.set('view engine','pug')
 app.set('views','./views')
@@ -11,17 +12,41 @@ app.get("/megapixel",function(req,res)
     var add=req.query.add || [];
     var str=path;
     if(Array.isArray(add))
-        add.forEach((val,num)=>{str+=val+"/"});
+        add.forEach((val,num)=>{str+="/"+val});
     else
-        str+=add+"/";
+        str+="/"+add;
     console.log(str);
-    
-    fs.readdir(str,(err,files)=>{
-       // console.log(files)
-            files.forEach((val,num)=>{if(val[0]=='.')files.splice(num,1)})
+
+    var mtype=mime.getType(str);
+    if(mtype!=null)
+        mtype=mtype.split('/')[0];
+
+    if(mtype=="image")
+        res.render("page",{files: [{path: str, type: mtype}], add: add});
+    else if(mtype=="video")
+        res.render("page",{files: [{path: str, type: mtype}], add: add});
+    else
+    {
+        fs.readdir(str,(err,files)=>{
+            // console.log(files)
+            var rindex=[];
+            files.forEach((val,num)=>{
+                if(val[0]=='.')
+                    rindex.push(num);
+                else
+                    files[num]={path: val, type: "folder"}            
+            })
+
+            rindex.forEach((val,num)=>{
+                files.splice(val,1);
+            })
+           // console.log(files);
             res.render("page",{files: files,add: add})
-    })
+        })
+    }
 })
 
-
+app.use("/media",(req,res)=>{
+    res.sendFile(decodeURIComponent(req.originalUrl))
+})
 app.listen(3000);
