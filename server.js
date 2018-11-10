9e5f7b741e36ee6a2028ca0e451c55c2f61bfcdc
satyamholmes <invinciblesatyam@gmail.com>
2018-11-10 13:09:37 +0530

    modified the way it used to request for particular files or folder

--- server.js
+++ server.js
@@ -4,69 +4,81 @@ var mime=require('mime')
 var archive=require('archiver')
 var app=express();
 var getSize=require('get-folder-size');
-var path="/media/satyam/funplace/megapixels";
+var path="/media/satyam/funplace/megapixels/";
 
 app.set('view engine','pug')
 app.set('views','./views')
 
-app.get("/megapixel",function(req,res)
-{
-    var add=req.query.add || [];
-    var str=path;
-    if(Array.isArray(add))
-        add.forEach((val,num)=>{str+="/"+val});
-    else
-        str+="/"+add;
-   // console.log(str);
+app.get("/files/:file",(req,res)=>{
+    res.sendFile(__dirname+"/views/"+req.params.file)
+})
 
-    var mtype=mime.getType(str);
+app.use("/megapixel",function(req,res)
+{
+   // var add=req.query.add || [];
+   // var str=path;
+   var str= req.originalUrl;
+   console.log(str);
+
+   str=str.replace("/megapixel","");
+   //  if(Array.isArray(add))
+   //  add.forEach((val,num)=>{str+="/"+val});
+   //else
+   //   str+="/"+add;
+   str=decodeURIComponent(str);
+   console.log(str);
+
+    var mtype=mime.getType(path+str);
     if(mtype)
         mtype=mtype.split('/')[0];
 
     if(mtype=="image")
-        res.render("page",{files: [{path: str, type: mtype}], add: add});
+        res.render("page",{files: [{name: str, type: mtype}], str: str});
     else if(mtype=="video")
-        res.render("page",{files: [{path: str, type: mtype}], add: add});
+        res.render("page",{files: [{name: str, type: mtype}], str: str});
     else
     {
-        fs.readdir(str,(err,files)=>{
-           //  console.log(files)
+        fs.readdir(path+str, (err,files)=>{
+          //   console.log(files)
             var rindex=[];
             files.forEach((val,num)=>{
                 if(val[0]=='.')
                     rindex.push(num);
                 else
                 {
-                    var mtype=mime.getType(str+"/"+val);
-                    if(mtype && (mtype.split("/")[0]=="image" || mtype.split("/")[0]=="video"))
-                    {           
-                        var stats=fs.statSync(str+"/"+val);
-                        files[num]={path: val, type: "folder", fsize: (stats.size/1024/1024).toFixed(2)};
-                    }
+                    //var mtype=mime.getType(str+"/"+val);
+                    //if(mtype && (mtype.split("/")[0]=="image" || mtype.split("/")[0]=="video"))
+                    var stats=fs.statSync(path+str+"/"+val);
+                    if(stats.isFile())
+                        files[num]={name: val, type: "folder", fsize: (stats.size/1024/1024).toFixed(2)};
                     else
-                        files[num]={path: val, type: "folder", fsize: "--"}
+                        files[num]={name: val, type: "folder", fsize: "--"}
                 }
             })
             if(rindex.length!=0)
                 files.splice(rindex[0],rindex.length);
             
             //console.log(files);
-            res.render("page",{files: files,add: add, str: str})
+            res.render("page",{files: files,str: str})
         })
     }
 })
 
-app.use("/media",(req,res)=>{
+app.use("/download",(req,res)=>{
     var url=req.originalUrl;
-    var mtype=mime.getType(url);
+    url=url.replace("/download","");
+    url=decodeURIComponent(url);
+    //console.log(url);
+    var mtype=mime.getType(path+url);
     if(mtype && (mtype.split("/")[0]=="image" || mtype.split("/")[0]=="video"))
-        res.sendFile(decodeURIComponent(req.originalUrl))
+        res.sendFile(path+url)
     else
     {
         var arch=archive('zip');
         arch.pipe(res);
-        arch.directory(url,"")
+        arch.directory(path+url,"")
         arch.finalize();
     }
 })
+
 app.listen(3000);
